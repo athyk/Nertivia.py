be269724619c82ccf3750429bf02f28f2d779640
FluxedScript <fluxedscript@gmail.com>
2021-05-30 13:53:57 +0100

    License

--- setup.py
+++ setup.py
@@ -5,14 +5,15 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 with open('requirements.txt') as f:
     requirements: list = f.read().splitlines()
-print(requirements)
+
 setuptools.setup(
     name='pna',  # How you named your package folder (MyLib)
     version='0.0.1',  # Start with a small number and increase it with every change you make
     license='MIT',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description='Python API Wrapper for Nertivia to make bots',  # Give a short description about your library
     long_description=long_description,
-    long_description_content_type ="text/markdown",
+    long_description_content_type="text/markdown",
+    url="https://github.com/FluxedScript/PNA/tree/main",
     author='FluxedScript',  # Type in your name
     keywords=['API Wrapper', 'SIMPLE', 'PYTHON', "nertivia"],  # Keywords that define your package best
     package_dir={"socketioN": "socketioN"},
@@ -33,7 +34,7 @@ setuptools.setup(
         # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         'Intended Audience :: Developers',  # Define that your audience are developers
         'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: MIT License',  # Again, pick a license
+        'License :: OSI Approved :: Apache Software License',  # Again, pick a license
         'Programming Language :: Python :: 3',  # Specify which pyhton versions that you want to support
     ],
     python_requires=">=3.6",
